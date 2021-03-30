ff288b65f892f7f68fa4533e7b38f9392fbc32cb
ParkSeungSu <cldkr0401@naver.com>
2021-03-30 15:00:06 +0900

    Today_study
    Module - module.exports =
                    require('');
    refactoring

--- main.js
+++ main.js
@@ -3,36 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-var template = {
-  html:function(title,list,body,control){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-    </head>
-    <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-    </body>
-    </html>
-    `;
-  },
-  list:function(fileList){
-    var list = '<ul>';
-    for( var i = 0 ; i<fileList.length;i++){
-      list+=`<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-    }
-    list = list+'</ul>';
-    return list;
-  }
-};
-
-
-
+var template = require('./template');
 
 var app = http.createServer(function(request,response){
 
---
+++ syntex/muse.js
@@ -0,0 +1,11 @@
+/*
+var M = {
+    v:'v',
+    f:function(){
+        console.log(this.v);
+    }
+};
+*/
+var part = require('./mpart');
+part.f();
+console.log(part);
